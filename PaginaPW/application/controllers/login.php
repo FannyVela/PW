8d6f1464db339af134fda9d46f332e2b4689d6e9
fannyvd97 <33985674+fannyvd97@users.noreply.github.com>
2018-11-13 10:08:17 +0100

    fd

--- PaginaPW/application/controllers/login.php
+++ PaginaPW/application/controllers/login.php
@@ -1,3 +1,13 @@
 <?php
     error_reporting(E_ALL ^ E_DEPRECATED);
+
+    class login extends CI_Controller{
+        public function _construct(){
+            parent::_construct();
+        }
+        
+        public function index() {
+            $this->load->view('login_view');
+        }
+    }
 ?>
