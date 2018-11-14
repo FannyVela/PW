a8ed66b022f9b27a244f6175d020eb4eb094c3cf
fannyvd97 <33985674+fannyvd97@users.noreply.github.com>
2018-11-14 15:16:51 +0100

    lj

--- PaginaPW/application/views/inicio_view.php
+++ PaginaPW/application/views/inicio_view.php
@@ -12,8 +12,10 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('plugins/OwlCarousel2-2.2.1/owl.theme.default.css'); ?>">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('plugins/OwlCarousel2-2.2.1/animate.css'); ?>">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('plugins/slick-1.8.0/slick.css'); ?>">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/main_styles.css'); ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/main.css'); ?>"> 
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/responsive.css'); ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/fontawesome-all.css'); ?>">
+        
     </head>
     
     <body>
@@ -23,18 +25,17 @@
                     <div class="container">
                         <div class="row">
                             <div class="col d-flex flex-row">
-                                <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="images/phone.png" alt=""></div>
-                                <?php
-                                echo "Actualmente logeado con: ". $_SESSION['username'];
-                                ?>
+                                <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="<?php echo base_url('images/phone.png') ?>" alt=""></div>+38 068 005 3570
+                             <!--   <?php 
+                                    echo "Actualmente logeado con: ". $_SESSION['username']; 
+                                ?> -->
                                 </div>
-                                <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="images/phone.png" alt=""></div>
-                                </div>
-                                <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="images/mail.png" alt=""></div><a href="mailto:fastsales@gmail.com">fastsales@gmail.com</a></div>
+                    
+                                <div class="top_bar_contact_item"><div class="top_bar_icon"><img src="<?php echo base_url('images/mail.png') ?>" alt=""></div><a href="mailto:fastsales@gmail.com">fastsales@gmail.com</a></div>
                                 <div class="top_bar_content ml-auto">
                                     <div class="top_bar_user">
-                                        <div class="user_icon"><img src="images/user.svg" alt=""></div>
-                                        <div><a href="<?= base_url().'index.php/registro'?>">Register</a></div>
+                                        <div class="user_icon"><img src="<?php echo base_url('images/user.svg') ?>" alt=""></div>
+                                        <div><a href="<?= base_url().'registro'?>">Register</a></div>
                                         <div><a href="#">Sign in</a></div>
                                     </div>
                                 </div>
@@ -43,6 +44,90 @@
                     </div>		
                 </div>
             </header>
+            
+            <!-- Header Main -->
+            <div class="header_main">
+                <div class="container">
+                    <div class="row">
+
+                        <!-- Logo -->
+                        <div class="col-lg-2 col-sm-3 col-3 order-1">
+                            <div class="logo_container">
+                                <div class="logo"><a href="#">TecnoPhone</a></div>
+                            </div>
+                        </div>
+
+                        <!-- Wishlist -->
+                        <div class="col-lg-4 col-9 order-lg-3 order-2 text-lg-left text-right" style="margin-left:50%;">
+                            <div class="wishlist_cart d-flex flex-row align-items-center justify-content-end">
+                                <!-- Cart -->
+                                <div class="cart">
+                                    <div class="cart_container d-flex flex-row align-items-center justify-content-end">
+                                        <div class="cart_icon">
+                                            <img src="<?php echo base_url('images/cart.png') ?>" alt="">
+                                            <div class="cart_count"><span>0</span></div>
+                                        </div>
+                                        <div class="cart_content">
+                                            <div class="cart_text"><a href="#">Cart</a></div>
+                                      <!--      <div class="cart_price">0€</div>-->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- Main Navigation -->
+
+            <nav class="main_nav">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+
+                            <div class="main_nav_content d-flex flex-row">
+                                
+                                <!-- Main Nav Menu -->
+
+                                <div class="main_nav_menu" style="margin-left: 40%;">
+                                    <ul class="standard_dropdown main_nav_dropdown">
+                                        <li><a href="<?= base_url().'inicio'?>">Inicio<i class="fas fa-chevron-down"></i></a></li>
+                                        <li class="hassubs">
+                                            <a href="#">Marcas<i class="fas fa-chevron-down"></i></a>
+                                            <ul>
+                                                <li>
+                                                    <a href="#">Menu Item<i class="fas fa-chevron-down"></i></a>
+                                                    <ul>
+                                                        <li><a href="#">Menu Item<i class="fas fa-chevron-down"></i></a></li>
+                                                        <li><a href="#">Menu Item<i class="fas fa-chevron-down"></i></a></li>
+                                                        <li><a href="#">Menu Item<i class="fas fa-chevron-down"></i></a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.html">Contacto<i class="fas fa-chevron-down"></i></a></li>
+                                    </ul>
+                                </div>
+
+                                <!-- Menu Trigger -->
+
+                                <div class="menu_trigger_container ml-auto">
+                                    <div class="menu_trigger d-flex flex-row align-items-center justify-content-end">
+                                        <div class="menu_burger">
+                                            <div class="menu_trigger_text">menu</div>
+                                            <div class="cat_burger menu_burger_inner"><span></span><span></span><span></span></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            
+            
         </div> 
     </body>
 </html>
